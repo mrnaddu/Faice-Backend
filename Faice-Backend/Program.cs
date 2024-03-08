a05a03d5eafa2e86c888de695b5425eb7ff871fb
mrnaddu <mohammadnadm5@gmail.com>
2024-03-08 20:53:52 +0530

    .

--- Faice-Backend/Program.cs
+++ Faice-Backend/Program.cs
@@ -1,11 +1,21 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = builder.Configuration;
 
-builder.Services.AddControllers();
+// Prevent json object from being self referenced multiple times when serialized to string
+builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -17,6 +27,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+// Add authentication stuff
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    Secure = CookieSecurePolicy.Always
+});
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
